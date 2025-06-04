d6ba9792e574bbc1f774b9e4fb767e9162283c14
risonakamo <hmykanae@gmail.com>
2025-06-04 17:45:31 -0400

    designing initial ui concept

--- web/pages/main/main.svelte
+++ web/pages/main/main.svelte
@@ -2,4 +2,35 @@
 
 </script>
 
-hello
+<style lang="sass">
+    @use "./main.sass"
+</style>
+
+<div class="container">
+    <div class="task-timer">
+        <p>current task: <span class="task-text">Doing Something</span></p>
+        <h3>00:15:13</h3>
+        <button class="stop-button">Stop</button>
+    </div>
+
+    <div class="task-adder">
+        <p>Start New Task</p>
+        <input type="text" list="task-auto-complete" class="task-input"/>
+
+        <datalist id="task-auto-complete">
+            <option value="Email follow-up">
+            <option value="Code review">
+            <option value="Sprint planning">
+            <option value="Bug fixing">
+            <option value="Design mockups">
+        </datalist>
+
+        <button class="start-button">Start</button>
+    </div>
+
+    <div class="time-table">
+        <div class="time-row">
+
+        </div>
+    </div>
+</div>
