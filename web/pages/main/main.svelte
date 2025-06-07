507cba31884838ea2b0e206aa401a3b3e5ecadf4
risonakamo <hmykanae@gmail.com>
2025-06-07 11:48:39 -0400

    ui designing

--- web/pages/main/main.svelte
+++ web/pages/main/main.svelte
@@ -7,25 +7,28 @@
 </style>
 
 <div class="container">
-    <div class="task-timer">
-        <p>current task: <span class="task-text">Doing Something</span></p>
-        <h3>00:15:13</h3>
-        <button class="stop-button">Stop</button>
-    </div>
-
     <div class="task-adder">
         <p>Start New Task</p>
         <input type="text" list="task-auto-complete" class="task-input"/>
 
         <datalist id="task-auto-complete">
-            <option value="Email follow-up">
-            <option value="Code review">
-            <option value="Sprint planning">
-            <option value="Bug fixing">
-            <option value="Design mockups">
+            <option value="Email follow-up"></option>
+            <option value="Code review"></option>
+            <option value="Sprint planning"></option>
+            <option value="Bug fixing"></option>
+            <option value="Design mockups"></option>
         </datalist>
 
         <button class="start-button">Start</button>
+    </div>
+
+    <div class="task-timer">
+        <p>current task: <span class="task-text">Doing Something</span></p>
+
+        <div class="timer">
+            <h3>00:15:13</h3>
+            <button class="stop-button">Stop</button>
+        </div>
     </div>
 
     <div class="time-table">
