efe3e2f9bcc8cada9f9a561112771fd550fde06f
risonakamo <hmykanae@gmail.com>
2025-06-07 12:04:44 -0400

    time table designing

--- web/pages/main/main.svelte
+++ web/pages/main/main.svelte
@@ -32,8 +32,105 @@
     </div>
 
     <div class="time-table">
-        <div class="time-row">
+        <div class="day-header">
+            <div class="title">
+                Sat, Jun 5 (Today)
+            </div>
 
+            <div class="duration">
+                02:04:19
+            </div>
+        </div>
+
+        <div class="time-row">
+            <div class="selection">
+                <input type="checkbox"/>
+            </div>
+
+            <div class="sub-rows">
+                1
+            </div>
+
+            <div class="title">
+                2b details
+            </div>
+
+            <div class="time-range">
+                23:23 - 23:39
+            </div>
+
+            <div class="duration">
+                00:15:06
+            </div>
+
+            <div class="buttons">
+                <button>▶</button>
+                <button>🗑</button>
+            </div>
+        </div>
+
+        <div class="time-row">
+            <div class="selection">
+                <input type="checkbox"/>
+            </div>
+
+            <div class="sub-rows">
+                1
+            </div>
+
+            <div class="title">
+                2b details
+            </div>
+
+            <div class="time-range">
+                23:23 - 23:39
+            </div>
+
+            <div class="duration">
+                00:15:06
+            </div>
+
+            <div class="buttons">
+                <button>▶</button>
+                <button>🗑</button>
+            </div>
+        </div>
+
+        <div class="day-header">
+            <div class="title">
+                Friday, Jun 4 (Yesterday)
+            </div>
+
+            <div class="duration">
+                01:19:12
+            </div>
+        </div>
+
+        <div class="time-row">
+            <div class="selection">
+                <input type="checkbox"/>
+            </div>
+
+            <div class="sub-rows">
+                1
+            </div>
+
+            <div class="title">
+                2a something
+            </div>
+
+            <div class="time-range">
+                23:23 - 23:39
+            </div>
+
+            <div class="duration">
+                00:15:06
+            </div>
+
+            <div class="buttons">
+                <button>▶</button>
+                <button>🗑</button>
+            </div>
         </div>
     </div>
 </div>
